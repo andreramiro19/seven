299e92fae251ebad53b0d6c5f3ebf00848943c15
Nayru <andreramiroapp@gmail.com>
2021-10-05 19:04:25 -0300

    primeiro commit

---
+++ assets/js/scroll-reveal.js
@@ -0,0 +1,94 @@
+// ======================================== OFERTAS ======================================== //
+var scrollOfertasTitle = {
+    delay: 100,
+    duration: 2200,
+    opacity: 0
+}
+
+var card1 = {
+    distance: '50%',
+    origin: 'left',
+    duration: 1500,
+    opacity: 0
+};
+
+var card2 = {
+    delay: 200,
+    distance: '50%',
+    origin: 'bottom',
+    duration: 1500,
+    opacity: 0
+};
+
+var card3 = {
+    delay: 400,
+    distance: '50%',
+    origin: 'right',
+    duration: 1500,
+    opacity: 0
+};
+
+// ======================================== HOME ======================================== //
+var scrollHomeBlob = {
+    distance: '100%',
+    origin: 'top',
+    duration: 2000,
+    opacity: 0
+}
+
+var scrollHomeImg = {
+    delay: 200,
+    distance: '100%',
+    origin: 'left',
+    duration: 1200,
+    opacity: 0
+};
+
+var scrollHomeAnim = {
+    delay: 200,
+    duration: 2200,
+    opacity: 0
+}
+
+var scrollHomeTitle = {
+    delay: 400,
+    duration: 1200,
+    opacity: 0
+};
+
+var scrollHomeSubtitle = {
+    delay: 1000,
+    duration: 1200,
+    opacity: 0
+};
+
+var scrollHomeButton = {
+    delay: 1200,
+    distance: '50%',
+    duration: 1200,
+    opacity: 0
+};
+
+var scrollHomeMouse = {
+    delay: 1200,
+    distance: '100%',
+    origin: 'top',
+    duration: 1500,
+    opacity: 0
+};
+
+// ==================== HOME ==================== //
+ScrollReveal().reveal('.scroll-home-blob', scrollHomeBlob);
+ScrollReveal().reveal('.scroll-home-title', scrollHomeTitle);
+ScrollReveal().reveal('.scroll-home-subtitle', scrollHomeSubtitle);
+ScrollReveal().reveal('.scroll-home-button', scrollHomeButton);
+ScrollReveal().reveal('.scroll-home-img', scrollHomeImg);
+ScrollReveal().reveal('.scroll-home-anim', scrollHomeAnim);
+ScrollReveal().reveal('.scroll-home-mouse', scrollHomeMouse);
+
+// ==================== OFERTAS ==================== //
+ScrollReveal().reveal('.scroll-ofertas--title', scrollOfertasTitle);
+ScrollReveal().reveal('.card1', card1);
+ScrollReveal().reveal('.card2', card2);
+ScrollReveal().reveal('.card3', card3);
+
---
+++ assets/js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView:'auto',
+    spaceBetween: 15,
+    grabCursor: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+  });
